831ec5653204679e41485bd299cc8fa5afabdfd9
theeseunbadejo <63941884+theeseunbadejo@users.noreply.github.com>
2020-04-21 11:46:23 +0100

    Update properties_controller.rb

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -35,7 +35,7 @@ class PropertiesController < ApplicationController
 
     respond_to do |format|
       if @property.save
-        property_path(uuid: #property.uuid)
+        property_path(uuid: @property.uuid)
         format.json { render :show, status: :created, location: @property }
       else
         format.html { render :new }
